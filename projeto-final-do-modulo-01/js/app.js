f681c4b1c5e5bd3e2a02cb4962388efb083daca6
João Paulo Calaça <58599256+Calaca-dev@users.noreply.github.com>
2023-04-12 03:06:47 -0300

    feat: creating two modal 's
    creating two modals with javaScript.

--- projeto-final-do-modulo-01/js/app.js
+++ projeto-final-do-modulo-01/js/app.js
@@ -5,3 +5,56 @@ function changeInputs() {
   showPaswordCard.style.display="block"
   
 }
+
+function changeArrowAndOpenModal() {
+  const iconArrowDown = document.querySelector('#arrowDownStyle');
+    const iconArrowUp = document.querySelector('#arrowUpStyle')
+    const dropDownIsOpen = iconArrowDown.style.display==='none';
+  
+      
+    if (dropDownIsOpen) {
+      iconArrowDown.style.display="inline-block";
+      iconArrowUp.style.display="none";
+
+      
+    }
+    else {
+      iconArrowDown.style.display="none";
+      iconArrowUp.style.display="inline-block";
+    }
+   
+    bubblueModalOpenClose();
+  }
+   
+
+  function bubblueModalOpenClose() {
+    const modalbubble = document.querySelector('.bubblueLogOut');
+    const dropDownIsOpen = modalbubble.style.display==='block';
+  
+    if (dropDownIsOpen){
+        modalbubble.style.display="none"
+    }
+    else {
+      modalbubble.style.display="block"
+    }
+
+
+  }
+
+
+  function modalInputsNewSubscribe() {
+    const modalContainer = document.querySelector('.newSubscribeModal');
+    const mainContainer = document.querySelector('.mainContent');
+    modalContainer.style.display==='none';
+    if (modalContainer) {
+      modalContainer.style.display='flex';
+      mainContainer.style.display='none';
+    }
+    else {
+      modalContainer.style.display='none';
+      mainContainer.style.display='flex';
+    }
+
+    console.log(mainContainer.style.display)
+    console.log(modalContainer.style.display)
+  }
